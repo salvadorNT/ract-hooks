38e00134927ee1172ea35b2dad8392b6b0c39d7a
salvador <salvadorjnt@gmail.com>
2024-06-08 14:14:09 -0600

    custom hook

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,10 +1,11 @@
-import { CounterApp } from "./01-useState/CounterApp"
+// import { CounterApp } from "./01-useState/CounterApp"
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook"
 
 export const HooksApp = () => {
     return (
         <>
             <h1>HooksApp</h1>
-            <CounterApp />
+            <CounterWithCustomHook />
         </>
     )
 }
