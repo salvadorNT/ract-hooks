eecd3331941bb21bb35c1f98cd3a5e608a84208e
salvador <salvadorjnt@gmail.com>
2024-06-11 17:21:55 -0600

    memo function

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -3,13 +3,15 @@
 // import { SimpleForm } from "./02-useEfect/SimpleForm"
 // import { FormWithCustomHook } from "./02-useEfect/FormWithCustomHook"
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks"
-import { FocusScreen } from "./04-useRef/FocusScreen"
+// import { FocusScreen } from "./04-useRef/FocusScreen"
+// import { Layout } from "./05-useLayoutEffect/Layout"
+import { Memorize } from "./06-memos/Memorize"
 
 export const HooksApp = () => {
     return (
         <>
             <h1>HooksApp</h1>
-            <FocusScreen />
+            <Memorize />
         </>
     )
 }
