d2425a5f4cd756d3e0148266e9e1f8292a456702
salvador <salvadorjnt@gmail.com>
2024-06-10 23:23:23 -0600

    useRef hook

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,45 @@
+import { useRef } from "react"
+
+export const FocusScreen = () => {
+
+    const inputRef = useRef()
+
+    const focusFirstInput = () => {
+        inputRef.current.select();
+    }
+
+    return (
+        <>
+            <h1>Focus Screen</h1>
+            <hr />
+
+            <input 
+            
+            ref={inputRef}
+            type="text"
+            placeholder="Ingrese su nombre"
+            className="form-control mt-2" />
+
+            <input 
+            type="text"
+            placeholder="Ingrese su nombre"
+            className="form-control mt-2" />
+
+            <input 
+            type="text"
+            placeholder="Ingrese su nombre"
+            className="form-control mt-2" />
+
+            <input 
+            type="text"
+            placeholder="Ingrese su nombre"
+            className="form-control mt-2" />
+
+            <button className="btn btn-primary mt-2"
+            onClick={focusFirstInput}>
+                Set Focus
+            </button>
+        </>
+
+    )
+}
--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -2,13 +2,14 @@
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook"
 // import { SimpleForm } from "./02-useEfect/SimpleForm"
 // import { FormWithCustomHook } from "./02-useEfect/FormWithCustomHook"
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks"
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks"
+import { FocusScreen } from "./04-useRef/FocusScreen"
 
 export const HooksApp = () => {
     return (
         <>
             <h1>HooksApp</h1>
-            <MultipleCustomHooks />
+            <FocusScreen />
         </>
     )
 }
