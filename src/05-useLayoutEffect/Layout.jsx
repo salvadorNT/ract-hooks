ee93a0b419e6bcf28be78cb0918ef6806da6e259
salvador <salvadorjnt@gmail.com>
2024-06-11 17:22:15 -0600

    pending layout lesson

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,49 @@
+import { useCounter } from "../hooks/useCounter";
+import { useFetch } from "../hooks/useFetch"
+import { LoadingMessage } from "../03-examples/LoadingMessage";
+import { PokemonCard } from "../03-examples/PokemonCard";
+import { useLayoutEffect } from "react";
+
+
+export const Layout = () => {
+
+    useLayoutEffect(() => {
+      
+    }, [])
+    const { counter, decrement, increment } = useCounter(1);
+    const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
+
+    return (
+        <>
+            <h1>Información de Pokémon</h1>
+            <hr />
+            {isLoading ? <LoadingMessage />
+                : <PokemonCard
+                    name={data.name}
+                    id={data.id}
+                    sprites={[
+                        data.sprites.front_default,
+                        data.sprites.back_default,
+                        data.sprites.front_shiny,
+                        data.sprites.front_shiny,
+                    ]}
+                />}
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+
+
+            <button
+                className="btn btn-primary mt-2"
+                onClick={counter > 1 ? decrement : null}
+            >
+                Anterior
+            </button>
+            <button className="btn btn-warning mt-2">{counter}</button>
+            <button
+                className="btn btn-primary mt-2"
+                onClick={increment}
+            >
+                Siguiente
+            </button>
+        </>
+    )
+}
