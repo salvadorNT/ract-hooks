3a666b948c4f0eb3ec24c3a33082540951aa1159
salvador <salvadorjnt@gmail.com>
2024-06-12 10:55:30 -0600

    memorize hw init

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -7,13 +7,14 @@
 // import { Layout } from "./05-useLayoutEffect/Layout"
 // import { Memorize } from "./06-memos/Memorize"
 // import { MemoHook } from "./06-memos/MemoHook"
-import { CallbackHook } from "./06-memos/CallbackHook"
+// import { CallbackHook } from "./06-memos/CallbackHook"
+import { Padre } from "./07-tarea-memo/Padre"
 
 export const HooksApp = () => {
     return (
         <>
             <h1>HooksApp</h1>
-            <CallbackHook />
+            <Padre />
         </>
     )
 }
