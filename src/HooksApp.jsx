60709d607011b7dc02e820a9f612a7966e3f40e7
salvador <salvadorjnt@gmail.com>
2024-06-11 20:01:04 -0600

    useMemo hook

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -5,13 +5,14 @@
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks"
 // import { FocusScreen } from "./04-useRef/FocusScreen"
 // import { Layout } from "./05-useLayoutEffect/Layout"
-import { Memorize } from "./06-memos/Memorize"
+// import { Memorize } from "./06-memos/Memorize"
+import { MemoHook } from "./06-memos/MemoHook"
 
 export const HooksApp = () => {
     return (
         <>
             <h1>HooksApp</h1>
-            <Memorize />
+            <MemoHook />
         </>
     )
 }
