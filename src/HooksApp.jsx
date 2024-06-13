74560f3ad4c8f552b6585fddddcc039ecf09e6cf
salvador <salvadorjnt@gmail.com>
2024-06-12 20:14:34 -0600

    reducer intro

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -8,13 +8,14 @@
 // import { Memorize } from "./06-memos/Memorize"
 // import { MemoHook } from "./06-memos/MemoHook"
 // import { CallbackHook } from "./06-memos/CallbackHook"
-import { Padre } from "./07-tarea-memo/Padre"
+// import { Padre } from "./07-tarea-memo/Padre"
+import  "./08-useReducer/intro-reducer";
 
 export const HooksApp = () => {
     return (
         <>
             <h1>HooksApp</h1>
-            <Padre />
+            
         </>
     )
 }
