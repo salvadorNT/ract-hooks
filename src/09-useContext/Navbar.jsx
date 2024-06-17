94437753ba78548b3260037190173fda1dd52e0d
salvador <salvadorjnt@gmail.com>
2024-06-17 10:32:02 -0600

    Created Navbar component

---
+++ src/09-useContext/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link, NavLink } from "react-router-dom"
+
+export const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary rounded-3" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">useContext</Link>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <NavLink className={ ({isActive}) => `nav-link ${ isActive ? 'active': ''}` } aria-current="page" to="/">Home</NavLink>
+                            <NavLink className={ ({isActive}) => `nav-link ${ isActive ? 'active': ''}` } to="/about">About</NavLink>
+                            <NavLink className={ ({isActive}) => `nav-link ${ isActive ? 'active': ''}` } to="/login">Login</NavLink>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
